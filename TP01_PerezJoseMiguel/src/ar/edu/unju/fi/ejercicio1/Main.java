9559dc2d16c10e045b9002e2816728548bd25123
miguel <miguel@localhost>
2024-04-05 23:25:48 -0300

    Ejercicio 01 del TP01 Programacion Visual

---
+++ TP01_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int primerNumero = 8, segundoNumero = 5, tercerNumero =1, cuartoNumero = 20, quintoNumero = 9;
+		float promedio = ((float)primerNumero + (float)segundoNumero + (float)tercerNumero + (float)cuartoNumero + (float)quintoNumero)/5;
+		System.out.println(primerNumero+", "+segundoNumero+", "+tercerNumero+", "+cuartoNumero+", "+quintoNumero);
+		System.out.println("El promedio es: "+ promedio);
+	}
+
+}
