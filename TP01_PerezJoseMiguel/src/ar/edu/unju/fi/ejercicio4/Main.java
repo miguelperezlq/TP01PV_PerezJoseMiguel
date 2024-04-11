e8cd0c67e778cdc242f742e8690fe7a843c9ee12
miguel <miguel@localhost>
2024-04-10 21:06:28 -0300

    Ejercicio 04

---
+++ TP01_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce un numero para calcular su factorial entre 1 y 9: ");
+		int numero = scanner.nextInt();
+		
+		if(numero<0 || numero> 9) {
+			System.out.println("Numero fuera del rango permitido");
+			return;
+		}
+		
+		int factorial = 1;
+		int i = 1;
+		
+		while(i <= numero) {
+			factorial *= i;
+			i++;
+		}
+		System.out.println("El factorial de " + numero+ "es: "+ factorial);
+		scanner.close();
+		
+
+	}
+
+}
