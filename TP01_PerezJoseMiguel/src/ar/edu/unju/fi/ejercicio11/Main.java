13fd0c6038fe47fa03d1b6c46ffe40d8244a80ca
miguel <miguel@localhost>
2024-04-12 20:27:37 -0300

    Ejercicio 11

---
+++ TP01_PerezJoseMiguel/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	 for (int i = 2, j = 40; j > 0; j--, i++) {
+             System.out.print(( i * j) + " ");
+         }
+    }
+}
